9e8cea7df1b52cef04a5cc0e8430c8bba199d3f4
Kaio Garcia <kaiofbgarcia@gmail.com>
2022-08-17 14:51:26 -0300

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from 'react-icons/fi';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+    
+      <div className="containerInput">
+        <input 
+          type="text"
+          placeholder="Digite seu CEP..."
+        />
+        <button className="buttonSearch">
+        <FiSearch size={25} color="#FFF"/>
+      </button>
+      </div>
+      <main className="main">
+        <h2>
+          CEP: 18870-546
+        </h2>
+        <span>
+          Bairo: Vila São Vicente
+        </span>
+        <span>
+          Rua: Dr Yves Ribeiro
+        </span>
+        <span>
+          Complemento: Apt 12
+        </span>
+        <span>
+          Bandeirantes - PR
+        </span>
+      </main>
     </div>
   );
 }
