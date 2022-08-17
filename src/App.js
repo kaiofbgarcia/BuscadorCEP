be08169772394eea1119c940eb74e84da301eebf
Kaio Garcia <kaiofbgarcia@gmail.com>
2022-08-17 15:29:02 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import './style.css';
+import api from './services/api';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCEP] = useState({});
+
+  async function handleSearch(){
+    if(input === ''){
+      alert("Insira algum CEP...");
+      return;
+    } 
+    try{
+      const respose = await api.get(`${input}/json`);
+      setCEP(respose.data);
+      setInput("");
+    } catch{
+      alert("Erro ao buscar!");
+      setInput("");
+    }
+  }
+  
   return (
     <div className="container">
       <h1 className="title">Buscador de CEP</h1>
@@ -10,28 +31,33 @@ function App() {
         <input 
           type="text"
           placeholder="Digite seu CEP..."
+          value={input}
+          onChange={(e)=> setInput(e.target.value)}
         />
-        <button className="buttonSearch">
-        <FiSearch size={25} color="#FFF"/>
-      </button>
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#FFF"/>
+        </button>
       </div>
-      <main className="main">
-        <h2>
-          CEP: 18870-546
-        </h2>
-        <span>
-          Bairo: Vila São Vicente
-        </span>
-        <span>
-          Rua: Dr Yves Ribeiro
-        </span>
-        <span>
-          Complemento: Apt 12
-        </span>
-        <span>
-          Bandeirantes - PR
-        </span>
-      </main>
+
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>
+            CEP: {cep.cep}
+          </h2>
+          <span>
+            Rua: {cep.logradouro}
+          </span>
+          <span>
+            Bairo: {cep.bairro}
+          </span>
+          <span>
+            DDD: {cep.ddd}
+          </span>
+          <span>
+            {cep.localidade} - {cep.uf}
+          </span>
+        </main>
+      )}
     </div>
   );
 }
